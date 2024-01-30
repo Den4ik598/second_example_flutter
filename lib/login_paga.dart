749a87f756c669269fa979b55888cd23bd81cadb
LenMArk <den.bulychev@inbox.ru>
2024-01-30 11:02:55 +0300

    first commit

---
+++ lib/login_paga.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: OverflowBar(
+              overflowSpacing: 20,
+              children: [
+                TextFormField(
+                  controller: _email,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Email is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: "Email",
+                      fillColor: Color.fromARGB(255, 172, 12, 201)),
+                ),
+                TextFormField(
+                  controller: _password,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Password is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: "Password",
+                      fillColor: Color.fromARGB(255, 172, 12, 201)),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print("Validation is done");
+                      }
+                    },
+                    child: const Text("Login"),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print("Validation is done");
+                      }
+                    },
+                    child: const Text("signUp"),
+                  ),
+                )
+              ],
+            ),
+          )),
+    ));
+  }
+}
